cdad8afc8e6cc5e6eec14b2db70755af04126fd0
weisernn <weisernn@users.noreply.github.com>
2024-03-19 20:18:05 +0800

    fix: get all from points

--- backend_web/src/controllers/points_controller.rs
+++ backend_web/src/controllers/points_controller.rs
@@ -1,13 +1,11 @@
 use axum::{extract::State, http::StatusCode, response::Json};
 use crate::{server::AppState, services::points_service};
+use crate::models::points_model::Points;
 
-pub async fn get_all_points(state: State<AppState>) -> Result<Json<Vec<i64>>, (StatusCode, String)> {
+pub async fn get_all_points(state: State<AppState>) -> Result<Json<Vec<Points>>, (StatusCode, String)> {
     let pool = state.pool.clone();
     let points = points_service::fetch_all_points(&pool)
         .await
-        .map(|points| {
-            points.iter().map(|point| point.points).collect::<Vec<i64>>() // Extract points field from Points struct
-        })
         .map(|points| Json(points)) // Convert to JSON
         .map_err(internal_error)?;
 
