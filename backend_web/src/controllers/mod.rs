27d26cc52996c57994cbd0f75c353e114fd99217
weisernn <weisernn@users.noreply.github.com>
2024-03-21 03:00:18 +0800

    clean

--- backend_web/src/controllers/mod.rs
+++ backend_web/src/controllers/mod.rs
@@ -3,5 +3,4 @@ pub mod manufacturer_controller;
 pub mod points_controller;
 pub mod rvm_controller;
 pub mod user_controllers;
-pub mod manufacturer_controller;
 pub mod bottle_transaction_controller;
