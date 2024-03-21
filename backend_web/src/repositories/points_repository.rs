e368f9d808a5845dd5808da8a18faab515fec3de
weisernn <weisernn@users.noreply.github.com>
2024-03-21 21:46:40 +0800

    edit: get top 50 highest points

--- backend_web/src/repositories/points_repository.rs
+++ backend_web/src/repositories/points_repository.rs
@@ -3,7 +3,7 @@ use sqlx::PgPool;
 
 pub async fn get_all_points(pool: &PgPool) -> Result<Vec<Points>, sqlx::Error> {
     // Example query to retrieve all users from the database
-    let points = sqlx::query_as::<_, Points>("SELECT * FROM points")
+    let points = sqlx::query_as::<_, Points>("SELECT * FROM points ORDER BY points DESC LIMIT 50")
         .fetch_all(pool)
         .await?;
     Ok(points)
