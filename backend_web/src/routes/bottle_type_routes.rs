d767824ff3c1cbe96f46b58d75391138edd8ff3b
Chan Zhe Xiang <zxchan.2021@scis.smu.edu.sg>
2024-04-03 12:18:36 +0800

    change /bottle_type/check to post request

--- backend_web/src/routes/bottle_type_routes.rs
+++ backend_web/src/routes/bottle_type_routes.rs
@@ -1,11 +1,11 @@
 use crate::{controllers::bottle_controllers, server::AppState};
-use axum::{routing::get, Router};
+use axum::{routing::post, Router};
 
 pub fn bottle_type_routes() -> Router<AppState> {
     println!("bottle_type_routes");
     // This route gets all the rows for the manufacturer
     Router::new().nest(
         "/bottle_type",
-        Router::new().route("/check", get(bottle_controllers::check_bottles)),
+        Router::new().route("/check", post(bottle_controllers::check_bottles)),
     )
 }
